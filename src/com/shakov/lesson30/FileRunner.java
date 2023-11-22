7e9c702b0296f2442eb31c5b9ddce5a87bb11b0d
Anton Shakov <annawinnern1@gmail.com>
2023-11-22 23:39:01 +0300

    file-DONE

---
+++ src/com/shakov/lesson30/FileRunner.java
@@ -0,0 +1,21 @@
+package com.shakov.lesson30;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileRunner {
+
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("resources/test.txt");
+        file.createNewFile();
+        System.out.println(file.exists());
+        System.out.println(file.isFile());
+        System.out.println(file.isDirectory());
+        System.out.println(file.getName());
+        System.out.println(file.getParent());
+        System.out.println(file.length());
+        System.out.println(file.canWrite());
+        System.out.println(file.getAbsolutePath());
+    }
+}
